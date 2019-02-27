4a55f54ebaea6ff1f0066905e098fabf518db602
leksyib <leksyib14@gmail.com>
2019-02-27 14:46:12 +0100

    chore(change production config object)

--- config/config.js
+++ config/config.js
@@ -16,17 +16,11 @@ const config = {
     dialect: 'postgres'
   },
   production: {
-    use_env_variable: 'DATABASE_URL'
-  },
-  heroku: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    ssl: true,
-    dialectOptions: {
-      ssl: {
-        require: true
-      }
-    }
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_TEST,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
   }
 };
 
