3413486c7d01622bff51733d188925930fbc8555
leksyib <leksyib14@gmail.com>
2019-02-27 14:22:46 +0100

    chore(Configure Authors Haven to use PostgreSQL as DB Engine)

---
+++ config/config.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+
+const config = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_TEST,
+    host: process.env.DB_HOST,
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL'
+  },
+  heroku: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    ssl: true,
+    dialectOptions: {
+      ssl: {
+        require: true
+      }
+    }
+  }
+};
+
+module.exports = config;
